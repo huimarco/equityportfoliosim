df608aca2e3fca37392d47d6747a817b6beb3af5
Marco Hui <marco01px2018@gmail.com>
2023-11-14 17:29:40 -0800

    cleaner way to ask/store input files

--- gui_utils.py
+++ gui_utils.py
@@ -2,7 +2,7 @@ import pandas as pd
 import tkinter as tk
 from tkinter import filedialog, simpledialog, messagebox
 
-def input_with_validation(title, prompt, validate, error_message):
+def validateInput(title, prompt, validate, error_message):
     while True:
         user_input = simpledialog.askstring(title, prompt)
         if user_input is None:
@@ -13,15 +13,15 @@ def input_with_validation(title, prompt, validate, error_message):
         else:
             messagebox.showerror('Error', error_message)
 
-def validate_date(date):
+def isDate(date):
     try:
         pd.to_datetime(date, format='%Y-%m-%d')
         return True
     except ValueError:
         return False
 
-def get_input_file_path(input_title):
+def getInputFilePath(input_title):
     return filedialog.askopenfilename(title=input_title, filetypes=[('Excel files', '*.xlsx')])
 
-def get_output_file_path():
+def getOutputFilePath():
     return filedialog.asksaveasfilename(title='Create the output Excel file', filetypes=[('Excel files', '*.xlsx')], defaultextension='.xlsx')
--- main.py
+++ main.py
@@ -5,37 +5,43 @@ from tkinter import filedialog, simpledialog, messagebox
 from tqdm import tqdm
 
 from portfolio_sim import runSim
-from gui_utils import input_with_validation, validate_date, get_input_file_path, get_output_file_path
+from gui_utils import validateInput, isDate, getInputFilePath, getOutputFilePath
 
 def main():
     root = tk.Tk()
     root.withdraw()  # Hide the root window
 
-    newsig_file_path = get_input_file_path('Select an Excel file of portfolio signals')
-    if not newsig_file_path:
-        print('No input file selected. Exiting.')
-        return
+    # dict to store input dialog messages
+    inputs = {
+        'newsig': 'Select an Excel file of portfolio signals',
+        'sp500': 'Select an Excel file of S&P500 prices'
+    }
 
-    newsig_df = pd.read_excel(newsig_file_path, sheet_name='prices', skiprows=1)
+    # dict to store dataframes
+    dfs = {}
+
+    for key, message in inputs.items():
+        file_path = getInputFilePath(message)
+        if not file_path:
+            print(f'No input file for {key} selected. Exiting.')
+            return
+        dfs[key] = pd.read_excel(file_path, sheet_name='prices', skiprows=1)
+
     # Ensure pricing data starts 4 days after signal post (so we have previous price data on day one)
-    newsig_df.drop([0, 1, 2, 3], axis=1, inplace=True)
-
-    sp500_file_path = get_input_file_path('Select an Excel file of S&P500 prices')
-    if not sp500_file_path:
-        print('No input file2 selected. Exiting.')
-        return
-
-    sp500_df = pd.read_excel(sp500_file_path, sheet_name='prices', skiprows=1)
-
-    start_date = input_with_validation('Input', 'Enter Start Date (YYYY-MM-DD):', validate_date, 'Invalid date format. Use YYYY-MM-DD.')
-
-    if start_date is None:
-        print('Start Date not provided. Exiting.')
-        return
+    dfs['newsig'].drop([0, 1, 2, 3], axis=1, inplace=True)
 
     while True:
-        end_date = input_with_validation('Input', 'Enter End Date (YYYY-MM-DD):',
-                                         lambda date: validate_date(date) and pd.to_datetime(date) >= pd.to_datetime(start_date, format='%Y-%m-%d'),
+        start_date = validateInput('Input', 'Enter Start Date (YYYY-MM-DD):', isDate, 'Invalid date format. Use YYYY-MM-DD.')
+
+        if start_date is not None:
+            break
+        else:
+            print('Start Date not provided. Exiting.')
+            return
+
+    while True:
+        end_date = validateInput('Input', 'Enter End Date (YYYY-MM-DD):',
+                                         lambda date: isDate(date) and pd.to_datetime(date) >= pd.to_datetime(start_date, format='%Y-%m-%d'),
                                          'Invalid End Date. It must be a valid date and greater than or equal to the Start Date.')
         if end_date is not None:
             break
@@ -51,9 +57,9 @@ def main():
             print('Buy Size not provided. Exiting.')
             return
 
-    portfolio_df, daily_df, monthly_df, returns_df, sold_df = runSim(newsig_df, sp500_df, start_date, end_date, buy_size)
+    portfolio_df, daily_df, monthly_df, returns_df, sold_df = runSim(dfs['newsig'], dfs['sp500'], start_date, end_date, buy_size)
 
-    output_excel_file = get_output_file_path()
+    output_excel_file = getOutputFilePath()
 
     if not output_excel_file:
         print('No output file selected. Exiting.')
